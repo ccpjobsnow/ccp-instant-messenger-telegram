5a16c9d63924357603800131ee5baadc5bdd21d8
Onias Vieira Junior <cds.ojunior@ifood3rd.com>
2022-05-30 00:41:18 -0300

    .

--- src/main/java/com/ccp/implementations/instant/messages/telegram/CcpInstantMessengerTelegram.java
+++ src/main/java/com/ccp/implementations/instant/messages/telegram/CcpInstantMessengerTelegram.java
@@ -1,5 +1,11 @@
 package com.ccp.implementations.instant.messages.telegram;
 
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 import com.ccp.constantes.CcpConstants;
 import com.ccp.decorators.CcpMapDecorator;
 import com.ccp.dependency.injection.CcpEspecification;
@@ -9,16 +15,20 @@ import com.ccp.especifications.http.CcpHttpRequester;
 import com.ccp.especifications.http.CcpHttpResponseType;
 import com.ccp.especifications.instant.messenger.CcpInstantMessenger;
 import com.ccp.exceptions.db.UnexpectedHttpStatus;
+import com.ccp.utils.Utils;
 
 @CcpImplementation
 public class CcpInstantMessengerTelegram implements CcpInstantMessenger {
 	@CcpEspecification
 	private CcpHttpRequester ccpHttp;
+	private Set<Long> locks = Collections.synchronizedSet(new HashSet<>());
 
 	@Override
 	public void sendMessageToSupport(String botToken, String message) {
-		// TODO Auto-generated method stub
-		
+		Long supportTelegram = Long.valueOf(System.getenv("SUPPORT_TELEGRAM"));
+		long naoResponderPraNinguem = 0L;
+
+		this.sendMessage(botToken, message, supportTelegram, naoResponderPraNinguem, CcpConstants.doNotEvaluate, CcpConstants.byPass);
 	}
 
 	@Override
@@ -47,14 +57,57 @@ public class CcpInstantMessengerTelegram implements CcpInstantMessenger {
 	}
 
 	@Override
-	public void sendSlowlyMessage(String botToken, String message, Long chatId, Long replyTo) {
-		// TODO Auto-generated method stub
+	public Long sendSlowlyMessage(String botToken, String message, Long chatId, Long replyTo, Predicate<String> isLockedUser, Consumer<String> lockUser) {
 		
+		while(this.locks.contains(chatId)) {
+			Utils.sleep(1);
+		}
+		
+		long firstTime = System.currentTimeMillis();
+		this.locks.add(chatId);
+		Long sendMessage = this.sendMessage(botToken, message, chatId, replyTo, isLockedUser, lockUser);
+		Utils.sleep(3000 - (int)(System.currentTimeMillis() - firstTime));
+		return sendMessage;
 	}
 
 	@Override
-	public void sendMessage(String botToken, String message, Long chatId, Long replyTo) {
-		// TODO Auto-generated method stub
+	public Long sendMessage(String botToken, String message, Long chatId, Long replyTo, Predicate<String> isLockedUser, Consumer<String> lockUser) {
+		if(message.trim().isEmpty()) {
+			return 0L;
+		}
+		String id = botToken + "_" + chatId;
+
+		if(isLockedUser.test(id)) {
+			return 0L;
+		}
+		String mensagem = message.replace("<p>", "\n").replace("</p>", " ").replace(",<br/>", " ");
+		
+		if(mensagem.length() > 4096) {
+			mensagem = mensagem.substring(0, 4096);
+		}
+		
+		String url = this.getBotToken(botToken)
+				+ "/sendMessage";
+		CcpHttpHandler ccpHttpHandler = new CcpHttpHandler(200, this.ccpHttp);
+		CcpMapDecorator body = new CcpMapDecorator()
+		.put("reply_to_message_id", replyTo)
+		.put("parse_mode", "html")
+		.put("chat_id", chatId)
+		.put("text", mensagem);
+		CcpMapDecorator response = ccpHttpHandler.executeHttpRequest(url, "POST", new CcpMapDecorator(), body, CcpHttpResponseType.singleRecord);
+
+		boolean ok = response.getAsBoolean("ok");
+		if(ok) {
+			Long newMessageId = response.getInternalMap("result").getAsLongNumber("message_id");
+			
+			return newMessageId;
+		}
+		Integer errorCode = response.getAsIntegerNumber("error_code");
+		boolean esteBotFoiBloqueado = Integer.valueOf(403).equals(errorCode);
+		if(esteBotFoiBloqueado) {
+			lockUser.accept(id);
+		}
+		return 0L;
 		
 	}
 
